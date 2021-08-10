9956fc727a7804ee4656881d4b54651ba642dad5
asaf <asafalmog119@gmail.com>
2021-08-10 14:39:29 +0300

    nav styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Navbar, Container, Nav, NavDropdown, NavItem } from 'react-bootstrap'
 // import { a } from "react-router-dom";
 import './nav.css'
 const NavPage = () => {
     const [active, setACtive] = useState('activea')
+    const [bgDark, setBgDark] = useState(`
+    linear-gradient(
+        90deg,
+        rgba(83, 91, 170, 1) 0%,
+        rgba(15, 36, 55, 1) 43%,
+        rgba(0, 0, 0, 1) 100%,
+        rgba(53, 70, 49, 0.2528361686471463) 100%
+      )`)
+    useEffect(() => {
+
+        window.addEventListener("scroll", handleScroll);
+
+
+        // if (window.scrollY > 20)
+        //     document.getElementById('navBar').style.background = '#212529'
+    })
+
+    const handleScroll = () => {
+        if (window.scrollY > 40) {
+            setBgDark('#212529')
+            // document.getElementById('navBar').style.background = '#212529'
+        } else {
+            setBgDark(`
+            linear-gradient(
+                90deg,
+                rgba(83, 91, 170, 1) 0%,
+                rgba(15, 36, 55, 1) 43%,
+                rgba(0, 0, 0, 1) 100%,
+                rgba(53, 70, 49, 0.2528361686471463) 100%
+              )`)
+        }
+    };
+
     const setActive = (e) => {
 
         // document.getElementsByClassName('collapse')[0].classList.remove('show')
@@ -13,7 +46,7 @@ const NavPage = () => {
     }
 
     return (
-        <Navbar variant="dark" bg="dark" expand="lg" className='col-sm-12'>
+        <Navbar variant="dark" bg="dark" style={{ background: bgDark }} expand="lg" className='col-sm-12' id='navBar'>
             {/* <Container > */}
             {/* <Navbar.Brand href="/" className='col-sm-1'>My Portfolio</Navbar.Brand> */}
             <Navbar.Brand href="/" className='col-sm-1'>Asaf Almog</Navbar.Brand>
