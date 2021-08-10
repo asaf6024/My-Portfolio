1669b29d2a2ff52817db2b8679a32ca033824b62
asaf <asafalmog119@gmail.com>
2021-08-10 14:44:13 +0300

    update

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,8 +4,9 @@ const Landing = () => {
     return (
         <div className='container-fluid landingDiv'>
             <div className='container' id='landing'>
-                <MDBAnimation type="fadeIn" delay=".5s" className="text-center" data-mdb-animation-start="onHover">
-                    <MDBRow>
+                <MDBRow>
+                    <MDBAnimation type="fadeIn" delay=".5s" className="text-center" data-mdb-animation-start="onHover">
+
                         <div className='col-md-6 col-sm-12 landingContent'>
                             <p style={{ fontSize: '20pt' }}> Hi, I'm Asaf!<br />
                                 I'm a <b>Front End Developer</b> who specializes in <br />
@@ -13,22 +14,25 @@ const Landing = () => {
                                 {/* Find details about my Websites, Skiils and Experience below */}
                             </p>
                         </div>
-                        <img className='col-md-6 col-sm-12 landingImg' src="img/Asaf2.png" alt='Asaf' />
+                    </MDBAnimation>
+                    <MDBAnimation type="fadeIn" delay="3.5s" className="text-center" data-mdb-animation-start="onHover">
 
-                        {/* <MDBAnimation type="fadeOut" delay="3.8s" className="text-center arrowDown" id="arrowDownA">
+                        <img className='col-md-6 col-sm-12 landingImg' src="img/Asaf2.png" alt='Asaf' />
+                    </MDBAnimation>
+                    {/* <MDBAnimation type="fadeOut" delay="3.8s" className="text-center arrowDown" id="arrowDownA">
                             <img className='col-md-6 col-sm-12 landingImg' src="img/Asaf2.png" alt='Asaf' />
                         </MDBAnimation>
                         <MDBAnimation type="fadeIn" delay="2.5s" className="text-center arrowDown" id="arrowDownA">
                             <img className='col-md-6 col-sm-12 landingImg' src="img/Asaf.png" alt='Asaf' />
                         </MDBAnimation> */}
 
-                        <MDBAnimation type="fadeIn" delay=".8s" className="text-center arrowDown" id="arrowDownA">
-                            <a href="#websites">
-                                <i className="arrow bounce text-center fas fa-angle-down"></i>
-                            </a>
-                        </MDBAnimation>
-                    </MDBRow>
-                </MDBAnimation>
+                    <MDBAnimation type="fadeIn" delay=".8s" className="text-center arrowDown" id="arrowDownA">
+                        <a href="#websites">
+                            <i className="arrow bounce text-center fas fa-angle-down"></i>
+                        </a>
+                    </MDBAnimation>
+                </MDBRow>
+
 
             </div>
 
