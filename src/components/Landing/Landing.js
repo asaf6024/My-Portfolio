1907af12d950126b654f567bc6a48cd6df1227a1
asaf <asafalmog119@gmail.com>
2021-11-24 11:46:10 +0200

    add a new section on the Header

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,22 +8,47 @@ const Landing = (props) => {
     return (
         <div className='container-fluid landingDiv'>
             <div className='container' id='landing'>
-                <MDBRow>
+                <MDBRow className='rowOfLanding'>
                     <div className='col-md-6 col-sm-12 landingContent'>
                         <p style={{ fontSize: '20pt' }}>
                             <MDBAnimation type="fadeIn" delay=".5s" className="text-center" data-mdb-animation-start="onHover">
 
                                 Hi, I'm Asaf!<br />
                             </MDBAnimation>
-                            <MDBAnimation type="fadeInLeft" delay="2s" className="text-center" data-mdb-animation-start="onHover">
+                            <MDBAnimation type="fadeInLeft" delay="1.5s" className="text-center" data-mdb-animation-start="onHover">
                                 I'm a <b>Front End Developer</b> that specializes in <br />
                                 <span className='font-italic'>React JS, JavaScript, Css</span> and more..<br />
                                 {/* Find details about my Websites, Skiils and Experience below */}
                             </MDBAnimation>
 
+                            <MDBAnimation type="fadeIn" delay="4.5s" className="text-center" data-mdb-animation-start="onHover">
+                                <div className='caracteristics'>
+                                    <div className='caracteristicsDivs'>
+                                        <p>2 Years of <br />Experince</p>
+                                        <i className="fas fa-briefcase fa-2x"></i>
+                                    </div>
+                                    <div className='caracteristicsDivs'>
+                                        <p>Great<br /> Self-Learning</p>
+                                        <i className="fas fa-book-reader fa-2x"></i>
+                                    </div>
+                                    <div className='caracteristicsDivs'>
+                                        <p>A Team Player</p>
+                                        <i className="far fa-handshake fa-2x"></i>
+                                    </div>
+                                    <div className='caracteristicsDivs'>
+                                        <p>Willing to<br /> Work Hard</p>
+                                        <i className="far fa-thumbs-up fa-2x"></i>
+                                    </div>
+
+
+                                </div>
+
+
+                            </MDBAnimation>
+
                         </p>
                     </div>
-                    <MDBAnimation type="fadeIn" delay="5s" className="text-center landingImgDiv" data-mdb-animation-start="onHover">
+                    <MDBAnimation type="fadeIn" delay="0.5s" className="text-center landingImgDiv" data-mdb-animation-start="onHover">
 
                         <img className='col-md-6 col-sm-12 landingImg' src="img/Asaf2.png" alt='Asaf' />
                     </MDBAnimation>
@@ -35,7 +60,7 @@ const Landing = (props) => {
                         </MDBAnimation> */}
 
 
-                    <MDBAnimation type="fadeIn" delay="5.5s" className="text-center arrowDown" id="arrowDownA">
+                    <MDBAnimation type="fadeIn" delay="6.5s" className="text-center arrowDown" id="arrowDownA">
                         <a href="#websites">
                             <i className="arrow bounce text-center fas fa-angle-down"
                                 onClick={() => {
