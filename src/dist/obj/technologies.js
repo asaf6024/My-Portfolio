dbbc8656630b863b0c32552873c61369b935d8c4
asaf <asafalmog119@gmail.com>
2021-12-03 12:26:51 +0200

    add sass and update landing content in mobile

--- src/dist/obj/technologies.js
+++ src/dist/obj/technologies.js
@@ -1,62 +1,66 @@
 const technologies = [
     {
         id: 1,
-        name: 'React JS',
-        src: ('../img/logos/react.png')
-    }
-    ,
-    {
-        id: 2,
-        name: 'JavaScript',
-        src: ('../img/logos/js.png')
+        name: 'HTML 5',
+        src: ('../img/logos/html.png')
     },
     {
-        id: 3,
-        name: 'Css3',
+        id: 2,
+        name: 'Css 3',
         src: ('../img/logos/css.png')
     },
     {
+        id: 3,
+        name: 'JavaScript ES6',
+        src: ('../img/logos/js.png')
+    },
+    {
         id: 4,
-        name: 'HTML5',
-        src: ('../img/logos/html.png')
+        name: 'React JS',
+        src: ('../img/logos/react.png')
     },
     {
         id: 5,
         name: 'React Hooks',
         src: ('../img/logos/hooks.png')
     },
+    // {
+    //     id: 7,
+    //     name: 'ES6',
+    //     src: ('../img/logos/es6.png')
+    // },
     {
-        id: 7,
-        name: 'ES6',
-        src: ('../img/logos/es6.png')
-    },
-    {
-        id: 7,
-        name: 'Redux',
-        src: ('../img/logos/redux.png')
-    },
-    {
-        id: 8,
-        name: 'Angular JS',
-        src: ('../img/logos/angular.png')
-    },
-    {
-        id: 9,
+        id: 6,
         name: 'Node JS',
         src: ('../img/logos/node.png')
     },
     {
+        id: 7,
+        name: 'SASS',
+        src: ('../img/logos/sass.png')
+    },
+    {
+        id: 8,
+        name: 'Redux',
+        src: ('../img/logos/redux.png')
+    },
+    {
         id: 9,
+        name: 'Angular JS',
+        src: ('../img/logos/angular.png')
+    },
+    {
+        id: 10,
         name: 'C#',
         src: ('../img/logos/c_sharp.png')
     },
     {
-        id: 10,
+        id: 11,
         name: 'MongoDB',
         src: ('../img/logos/mongodb.png')
     },
     {
-        id: 11,
+        id: 12,
         name: 'SQL Server',
         src: ('../img/logos/sql.png')
     },
