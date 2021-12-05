d4df542950ca43cea37dee1fc6926b297b3236cb
asaf <asafalmog119@gmail.com>
2021-12-05 12:31:25 +0200

    Update Footer.js
    add link to github

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,8 @@ import './footer.css'
 const Footer = () => {
     return (
         <div className="footer">
-            <p>This site created by @Asaf Almog 2021</p>
+            <p>This site created by
+                <a href='https://github.com/asaf6024' target='_blank'> @Asaf Almog</a> 2021</p>
         </div>
     )
 }
