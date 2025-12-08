c5f7325acb733facbe06da27d144f9db15c22cde
asaf <asafalmog119@gmail.com>
2025-12-08 10:18:26 +0200

    redirect to new portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import React, { useState, useEffect } from 'react';
-import Nav from './components/Nav/Nav'
-import Home from './pages/Home/Home'
+import React, { useState, useEffect } from "react";
+import Nav from "./components/Nav/Nav";
+import Home from "./pages/Home/Home";
 
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
+
+// Redirect immediately before rendering anything
+window.location.replace("https://asafalmog.dev/");
+
 function App() {
-  const [navItemCicked, setNavItemCicked] = useState(null)
+  const [navItemCicked, setNavItemCicked] = useState(null);
 
   useEffect(() => {
     // window.scroll(0, 0)
-  })
+  });
 
   const setFunction = (item) => {
-    setNavItemCicked(item)
-    console.log('navItemCicked', navItemCicked)
-  }
+    setNavItemCicked(item);
+    console.log("navItemCicked", navItemCicked);
+  };
   return (
-    <div className='container-fluid'>
+    <div className="container-fluid">
       {/* <Router> */}
 
       <Nav setFunction={setFunction} />
@@ -29,16 +33,12 @@ function App() {
       <Route exact path="/websites" component={Websites} />
       <Route exact path="/skills" component={Skiils} /> */}
 
-
       {/* </Switch> */}
       {/* </Router> */}
       <Footer />
       {/* <NavigationBtn /> */}
 
-
       {/* </Router> */}
-
-
     </div>
   );
 }
